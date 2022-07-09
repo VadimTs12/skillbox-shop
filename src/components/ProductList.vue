35da2f563a5f7de3875f2994e31a4c9cb9e10683
Вадим Цуркану <tsurkanuvadim92@gmail.com>
2022-07-09 14:49:44 +0300

    create project

---
+++ src/components/ProductList.vue
@@ -0,0 +1,19 @@
+<template>
+  <ul class="catalog__list">
+   <ProductItem v-for="(product, index) in products" :key="index" :product="product"/>
+  </ul>
+</template>
+
+<script>
+import ProductItem from '@/components/ProductItem.vue';
+
+export default {
+  name: 'ProductList',
+  components: { ProductItem },
+  props: ['products'],
+};
+</script>
+
+<style scoped>
+
+</style>
