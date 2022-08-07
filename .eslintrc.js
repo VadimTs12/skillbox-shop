adbb6d3fd32e956897e46ef4726b6c4320415638
Вадим Цуркану <tsurkanuvadim92@gmail.com>
2022-08-07 11:40:19 +0300

    добавил чтоб были считаны getters и state в vuex модули

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,5 +14,6 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'max-len': ['error', 140],
+    'no-shadow': ['error', { allow: ['state', 'getters'] }],
   },
 };
