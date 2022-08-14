5b9c923ffbac2d06a56f103343bf317e5a039471
Вадим Цуркану <tsurkanuvadim92@gmail.com>
2022-08-14 20:53:18 +0300

    Добавление компонента BaseFormInput.vue и BaseFormTextarea.vue

---
+++ src/components/BaseFormInput.vue
@@ -0,0 +1,28 @@
+<template>
+  <label class="form__label">
+    <input class="form__input" v-model="dataValue" :type="type" name="address" :placeholder="placeholder"/>
+    <span class="form__value">{{ title }}</span>
+    <span class="form__error" v-if="error">{{ error }}</span>
+  </label>
+</template>
+
+<script>
+export default {
+  name: 'BaseFormInput',
+  props: ['title', 'type', 'error', 'placeholder', 'value'],
+  computed: {
+    dataValue: {
+      get() {
+        return this.value;
+      },
+      set(value) {
+        this.$emit('input', value);
+      },
+    },
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/BaseFormTextarea.vue
@@ -0,0 +1,28 @@
+<template>
+  <label class="form__label">
+              <textarea v-model="dataValue" class="form__input form__input--area" :name="name"
+                        placeholder="Ваши пожелания"></textarea>
+    <span class="form__value">Комментарий к заказу</span>
+  </label>
+</template>
+
+<script>
+export default {
+  name: 'BaseFormTextarea',
+  props: ['name'],
+  computed: {
+    dataValue: {
+      get() {
+        return this.value;
+      },
+      set(value) {
+        this.$emit('input', value);
+      },
+    },
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
