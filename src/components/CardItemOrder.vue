cf75914a9a6aa8aaa6ba5cc655453d27f34a8ffb
Вадим Цуркану <tsurkanuvadim92@gmail.com>
2022-08-14 20:57:41 +0300

    Создание компонента CartItemOrder

---
+++ src/components/CardItemOrder.vue
@@ -0,0 +1,24 @@
+<template>
+  <li class="cart__order">
+    <h3>{{ item.product.title }}</h3>
+    <b>{{ item.product.price  | numberFormat }} ₽</b>
+    <span>{{ item.productId }}</span>
+  </li>
+</template>
+
+<script>
+
+import numberFormat from '@/helpers/numberFormat';
+
+export default {
+  name: 'CardItemOrder',
+  props: ['item'],
+  filters: {
+    numberFormat,
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
